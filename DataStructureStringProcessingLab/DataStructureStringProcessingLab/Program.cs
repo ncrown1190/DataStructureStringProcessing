3681a91fea02122062dcb2425867012739d17c16
ncrown1190 <ncrown88@outlook.com>
2024-11-29 15:39:07 -0500

    initial commit

---
+++ DataStructureStringProcessingLab/DataStructureStringProcessingLab/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Please enter a word to reverse: ");
+string userWord = Console.ReadLine();
+
+Console.WriteLine(ReverseString(userWord));
+
+Console.WriteLine(Reverse(userWord));
+
+//string str = "Alligator";
+//Console.WriteLine(ReverseString(str));
+//Console.WriteLine(Reverse(str));
+
+
+//Without using a stack
+static string Reverse(string str)
+{
+    char[] charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+
+//With using a stack
+static string ReverseString(string str)
+{
+    Stack<char> chars = new Stack<char>();
+
+    foreach (char c in str)
+    {
+        chars.Push(c);
+    }
+
+    str = "";
+    while (chars.Count != 0)
+    {
+        str += chars.Peek();
+        chars.Pop();
+    }
+    return str;
+}
